a7d974d088b7fd88b6cddc607738b6cff9337ceb
GraziArcH <graziela@archoffice.tech>
2025-08-03 18:34:18 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 
+import { securityFacade, userFacade, companyFacade } from '@/application/factories'
+
+export * from '@/domain/user'
+export * from '@/domain/security'
+export * from '@/domain/shared'
+
+export {
+  userFacade,
+  securityFacade,
+  companyFacade
+}
